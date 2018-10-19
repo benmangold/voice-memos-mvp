49122cd7aecb9538f8af4c7e05941a9d7b43e30f
Ben Mangold <benmangold@gmail.com>
2018-10-19 11:57:37 -0400

    react boilerplate, webpack, serving static html from dist

---
+++ react-client/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audioMemos = []
+    }
+  }
+
+  render() {
+    return(<div>HELLO WORLD</div>)
+  }
+}
+
+ReactDom.render(<App/>, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 let app = express();
 
 app.use(express.static(path.join(__dirname,'../public')))
+app.use(express.static(__dirname + '/../react-client/dist'));
+
 
 app.listen(3000, () => {
   console.log('app is listening')
