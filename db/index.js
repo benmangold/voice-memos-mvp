bc8fdeac1f43abd88bd2aca56138cd77a66991f9
Ben Mangold <benmangold@gmail.com>
2018-10-20 13:29:41 -0400

    hardcoding db

--- db/index.js
+++ db/index.js
@@ -1,5 +1,7 @@
 let mongoose = require('mongoose');
-mongoose.connect(process.env.DB_PROVIDER || 'mongodb://127.0.0.1:27017/memos')
+// mongoose.connect(process.env.DB_PROVIDER || 'mongodb://127.0.0.1:27017/memos')
+mongoose.connect(process.env.DB_PROVIDER || 'mongodb://btm:hackreactor18@ds215822.mlab.com:15822/memos')
+
 
 let db = mongoose.connection;
 db.on('error', () => console.log('mongoose connection error'))
