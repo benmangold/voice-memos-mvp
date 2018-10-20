11e096f45009f1bca016747f01aae4aaff90071a
Ben Mangold <benmangold@gmail.com>
2018-10-20 14:20:41 -0400

    give audio elements unique key

--- react-client/src/components/MemoList.jsx
+++ react-client/src/components/MemoList.jsx
@@ -10,7 +10,7 @@ class MemoList extends React.Component {
     return (<div>
       { this.props.memoList.map(({ url, title, _id }, index, collection) => {
         return <div key={ index }>{ title }<br/>
-                  <audio controls>
+                  <audio key={url} controls>
                     <source src={ url } type="audio/mpeg"></source>
                   </audio>
                   <button onClick= { () => { this.props.onDelete(_id) } } >Delete</button>
