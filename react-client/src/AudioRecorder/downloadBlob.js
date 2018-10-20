d2464eaed28c9ab64099db8b19284e120ec30dcd
Ben Mangold <benmangold@gmail.com>
2018-10-20 11:01:46 -0400

    no need to save blob to dom

--- react-client/src/AudioRecorder/downloadBlob.js
+++ react-client/src/AudioRecorder/downloadBlob.js
@@ -5,20 +5,16 @@ const postBlobToServer = function(blob) {
     var fd = new FormData();
     fd.append('upl', blob, 'blobby.wav');
     
-    fetch('/memos/blob',
-    {
-        method: 'post',
-        body: fd
-    }); 
+    fetch('/memos/blob', { method: 'post', body: fd }); 
 }
 
 export default function downloadBlob(blob, filename) {
-    var url = window.URL.createObjectURL(blob);
-    var click = document.createEvent('Event');
+    // var url = window.URL.createObjectURL(blob);
+    // var click = document.createEvent('Event');
     // click.initEvent('click', true, true);
-    var link = document.createElement('A');
-    link.href = url;
-    link.download = filename;
+    // var link = document.createElement('A');
+    // link.href = url;
+    // link.download = filename;
     // link.dispatchEvent(click);
     // link.click();
 
